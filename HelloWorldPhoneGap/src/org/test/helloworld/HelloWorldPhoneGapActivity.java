99b93691d3083f69332f1b15364fc79373a9546d
Khalid Aziz <kaziz@u.northwestern.edu>
2011-09-27 16:42:05 -0500

    Initial PhoneGap HelloWorld

---
+++ HelloWorldPhoneGap/src/org/test/helloworld/HelloWorldPhoneGapActivity.java
@@ -0,0 +1,13 @@
+package org.test.helloworld;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class HelloWorldPhoneGapActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
