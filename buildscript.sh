74d4d4c610e151bb6c48b8cf2a091adfe4705098
Khalid Aziz <kaziz@u.northwestern.edu>
2011-10-02 23:40:45 -0500

    Updated buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -6,7 +6,7 @@
 ## Emulator installation is not tested here atm, mostly because I'm not sure how to make the script wait until the emulator is finished launching before installing
 
 echo Updating project ...
-../android-sdk-linux_x86/tools/android update project --target 1 --name HelloWorldPhoneGap --path HelloWorldPhoneGap/
+../android-sdk-linux_x86/tools/android update project --target 'android-10' --name HelloWorldPhoneGap --path HelloWorldPhoneGap/
 
 #echo Trying to create new emulator named SecondDevice ...
 #../android-sdk-linux_x86/tools/android create avd -n SecondDevice -t 1 --force
