f90f012ad3fe6fdbf230788df1d33fe9f909fe7c
Khalid Aziz <kaziz@u.northwestern.edu>
2011-10-06 21:52:18 -0500

    Some basic tests for a calculator function

--- HelloWorldPhoneGap/assets/www/helloworld.js
+++ HelloWorldPhoneGap/assets/www/helloworld.js
@@ -1,3 +1,39 @@
+// Khalid's functions
+
 function helloWorld() {
 	return "Hello world!";
 }
+
+function calculator(oper, num1, num2) {
+
+var result;
+
+if (oper == 'add') {
+	result = num1 + num2;
+}
+else if (oper == 'sub') {
+	result = num1 - num2;
+}
+else if (oper == 'mult') {
+	result = num1 * num2;
+}
+else if (oper == 'div') {
+
+if (num2 == 0)
+	return "num2 cannot be 0 in a div operation"
+	result = num1 / num2;
+}
+else {
+}
+
+return result;
+
+}
+
+// Cassie's functions
+
+// Dhruv's functions
+
+// Haotian's functions
+
+// PY's functions
--- jasmine-testing-files/spec/helloworldtest.js
+++ jasmine-testing-files/spec/helloworldtest.js
@@ -3,3 +3,15 @@ describe("Hello world", function() {
 		expect(helloWorld()).toEqual("Hello world!");
 	});
 });
+
+describe("Calculator", function() {
+	it("1x2=10", function() {
+		expect(calculator('mult',1,2)).toEqual(2);
+	});
+	it("0x10=10", function() {
+		expect(calculator('mult',0,10)).toEqual(0);
+	});
+	it("10/0=undef", function() {
+		expect(calculator('div',10,0)).toEqual("num2 cannot be 0 in a div operation");
+	});
+});
