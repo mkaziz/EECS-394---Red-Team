c19f4d39e920f0161f43736320ba29bd3aae10c6
Cassandra Rommel <cassandra.rommel@gmail.com>
2011-10-07 01:04:58 -0500

    Cassie's Test Edits

--- jasmine-testing-files/spec/helloworldtest.js
+++ jasmine-testing-files/spec/helloworldtest.js
@@ -15,3 +15,18 @@ describe("Calculator", function() {
 		expect(calculator('div',10,0)).toEqual("num2 cannot be 0 in a div operation");
 	});
 });
+
+describe ("AddingIsFun", function() {
+
+	it("1+2=3",function() {
+		expect(calculator('add',1,2)).toEqual(3);
+	});
+	it("1+3=4",function() {
+		expect(calculator('add',1,3)).toEqual(4);
+	});
+	it("1+4=5",function() {
+		expect(calculator('add',1,4)).toEqual(5);
+	});
+	
+
+});
