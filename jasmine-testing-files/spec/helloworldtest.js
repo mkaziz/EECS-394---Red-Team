b9055c24cb4d038a92e336c36ac11d5d0373754a
Dhruv Koul <dhruvkoul.2013@gmail.com>
2011-10-06 23:43:53 -0500

    Dhruv's Jasmine JavaScript tests for Assignment 3, Part 1 have been added to the github.

--- jasmine-testing-files/spec/helloworldtest.js
+++ jasmine-testing-files/spec/helloworldtest.js
@@ -15,3 +15,23 @@ describe("Calculator", function() {
 		expect(calculator('div',10,0)).toEqual("num2 cannot be 0 in a div operation");
 	});
 });
+
+describe("returnText", function()
+{
+	it("testing", function() {
+		expect(returnText("Chicago")).toEqual("The parameter passed to this function says Chicago.");
+	});
+});
+
+describe("Calculator2", function()
+{
+	it("(1/3)/(1/0)=0", function() {
+		expect(calculator2('div', 0, 0)).toEqual("Can't do this, can't have 0 in the denominator!");
+	});
+	it("(2*2)+(3*3)=13", function() {
+		expect(calculator2('sumsq', 2, 3)).toEqual(13);
+	});
+	it("(1/2)*(1/4)=(1/8)", function() {
+		expect(calculator2('mult', 2, 4)).toEqual(1/8);
+	});
+});
