6ecc1991030445ad2a6ded0c7a0832a752151edd
Khalid Aziz <kaziz@u.northwestern.edu>
2011-10-18 02:51:14 -0500

    Removed redundant HelloWorld Project, modified builscript.

--- buildscript.sh
+++ buildscript.sh
@@ -6,14 +6,16 @@
 ## Emulator installation is not tested here atm, mostly because I'm not sure how to make the script wait until the emulator is finished launching before installing
 
 echo Updating project ...
-../android-sdk-linux_x86/tools/android update project --target 'android-10' --name HelloWorldPhoneGap --path HelloWorldPhoneGap/
+../android-sdk-linux_x86/tools/android update project --target 'android-10' --name NotePad --path NotePad/
+../android-sdk-linux_x86/tools/android update project --target 'android-10' --name NuNumbers --path NuNumbers/
 
 #echo Trying to create new emulator named SecondDevice ...
 #../android-sdk-linux_x86/tools/android create avd -n SecondDevice -t 1 --force
 
 echo Trying new debug build
 #ant clean -f HelloWorldPhoneGap/build.xml
-ant debug -f HelloWorldPhoneGap/build.xml
+ant debug -f NotePad/build.xml
+ant debug -f NuNumbers/build.xml
 
 echo Running Jasmine tests ...
 cd jasmine-testing-files
