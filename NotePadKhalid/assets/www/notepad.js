8289466bbcb4e2d605c50596124508512e297722
Khalid Aziz <kaziz@u.northwestern.edu>
2011-10-18 06:08:50 -0500

    Successfully extracting data from note form to write to db.

--- NotePadKhalid/assets/www/notepad.js
+++ NotePadKhalid/assets/www/notepad.js
@@ -1,27 +1,47 @@
 
     // Wait for PhoneGap to load
     //
-    document.addEventListener("deviceready", onDeviceReady, false);
+    //document.addEventListener("deviceready", onDeviceReady, false);
 
-    // Populate the database 
-    //
-    function saveNote(tx) {
+    function checkDb(tx) {
+		var noteName = $("#name").val();
+		
+		if (noteName == "") {
+			alert("You must enter a name for your note!");
+			throw new exception();
+		}
+		
         try {
-	        tx.executeSql('DROP TABLE IF EXISTS notes');
+	        //tx.executeSql('DROP TABLE IF EXISTS notes');
 	        tx.executeSql('CREATE TABLE IF NOT EXISTS notes (note_id integer primary key, name varchar(200) unique, data text, save_time date default CURRENT_TIMESTAMP)');
-	        tx.executeSql('INSERT INTO notes (name, data) VALUES ("First Note","test text for note")');
-        	tx.executeSql('SELECT * FROM notes', [], querySuccess, errorCB);
+	        tx.executeSql('SELECT COUNT(*) AS count FROM notes WHERE name = "'+ noteName +'"', [], saveNote, errorCB);
 	     } catch (e) {
 	     	alert(e.message());
 	     }
     }
+    
+    function saveNote(tx, results) {
+		
+		var numberOfRecords = results.rows.item(0).count;		
+		var noteName = $("#name").val();
+		var noteData = $("#data").val();
+		
+		if (numberOfRecords == 0)
+			tx.executeSql('INSERT INTO notes (name, data) VALUES ("'+noteName+'","'+noteData+'")');
+		else if (numberOfRecords == 1)
+			tx.executeSql('UPDATE notes SET data = "'+ noteData +'", save_time = DateTime("now") WHERE name = "'+ noteName +'"');
+		else
+			alert("DB Error: More than one Note exists with this name.");
+		
+		tx.executeSql('SELECT * FROM notes', [], querySuccess, errorCB);
+		alert("Note saved");
+    }
 
-    function querySuccess(tx, results) {
-        
+    function querySuccess(tx, results) { 
         var len = results.rows.length;
 
         for (var i=0; i<len; i++){
-            alert("Row = " + i + " ID = " + results.rows.item(i).note_id + " Name =  " + results.rows.item(i).name + " time = " + results.rows.item(i).save_time);
+            alert("Row = " + i + " ID = " + results.rows.item(i).note_id + " Name =  " + results.rows.item(i).name + " Data =  " + results.rows.item(i).data + " time = " + results.rows.item(i).save_time);
         }
     }
 
@@ -40,5 +60,6 @@
     
     function onSave() {
         var db = window.openDatabase("notes", "1.0", "Notes Demo", 2000000);
-        db.transaction(saveNote, errorCB, successCB);
+        //db.transaction(checkDb, errorCB, successCB);
+        db.transaction(checkDb);
     }
