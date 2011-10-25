96ea3183106b163d27693833d52434a9ad209935
Haotian Liu <haotianliu@wireless-165-124-211-18.nuwlan.northwestern.edu>
2011-10-25 16:08:02 -0500

    Add GPS and URL data
    Add GPS and URL data for every food court
    Delete a court that no longer existing

--- food/assets/www/food.js
+++ food/assets/www/food.js
@@ -1,6 +1,19 @@
 var loclatitude;
 var loclongitude;
 
+var courtloc=new Array(
+	42.061,-87.675,"http://nucuisine.com/retail/lisas.html",
+	42.058,-87.676,"http://nucuisine.com/retail/tech_express.html",
+	42.057,-87.678,"http://nucuisine.com/retail/greatroom.html",
+	42.057,-87.673,"http://nucuisine.com/retail/einstein.html",
+	42.053,-87.672,"http://www.nucuisine.com/retail/norris.html",
+	42.053,-87.674,"http://www.nucuisine.com/retail/plaza.html",
+	42.053,-87.678,"http://www.nucuisine.com/retail/foster.html",
+	42.051,-87.675,"http://www.nucuisine.com/retail/crowe.html",
+	42.052,-87.681,"http://www.nucuisine.com/retail/frans.html",
+	42.050,-87.675,"http://www.nucuisine.com/retail/hinman.html",
+	);
+
 function GPSonSuccess(position) {
 	var element = document.getElementById('geolocation');
 	element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
