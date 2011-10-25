cc9cac7deb4cf7952f74bfd249a7c06fec19e660
Haotian Liu <haotianliu@wireless-165-124-211-18.nuwlan.northwestern.edu>
2011-10-25 15:30:04 -0500

    Add basic GPS feature
    Localize the jQuery js and css
    unload getting GPS

--- food/assets/www/food.js
+++ food/assets/www/food.js
@@ -1,3 +1,28 @@
+var loclatitude;
+var loclongitude;
+
+function GPSonSuccess(position) {
+	var element = document.getElementById('geolocation');
+	element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+						'Longitude: '          + position.coords.longitude             + '<br />' +
+						'Altitude: '           + position.coords.altitude              + '<br />' +
+						'Accuracy: '           + position.coords.accuracy              + '<br />' +
+						'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+						'Heading: '            + position.coords.heading               + '<br />' +
+						'Speed: '              + position.coords.speed                 + '<br />' +
+						'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+	loclatitude=position.coords.latitude;
+	loclongitude=position.coords.longitude;
+}
+
+function GPSonError(error) {
+	alert(	'code: '    + error.code    + '\n' +
+			'message: ' + error.message + '\n');
+}
+var locdistance = function(x,y){
+	return Math.sqrt((x-loclatitude)*(x-loclatitude)+(y-loclongitude)*(y-loclongitude));
+}
+
 function autoselect(){
 	document.getElementById("foodcourt4").selected=true;
 }
