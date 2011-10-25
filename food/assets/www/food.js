c99533f8ffdbfb2f84f276b36005689eeca9b5e5
Haotian Liu <haotianliu@wireless-165-124-211-18.nuwlan.northwestern.edu>
2011-10-25 16:23:20 -0500

    Able to find the nearest one

--- food/assets/www/food.js
+++ food/assets/www/food.js
@@ -1,18 +1,17 @@
 var loclatitude;
 var loclongitude;
 
-var courtloc=new Array(
+var courtinfo=new Array(
 	42.061,-87.675,"http://nucuisine.com/retail/lisas.html",
 	42.058,-87.676,"http://nucuisine.com/retail/tech_express.html",
 	42.057,-87.678,"http://nucuisine.com/retail/greatroom.html",
 	42.057,-87.673,"http://nucuisine.com/retail/einstein.html",
-	42.053,-87.672,"http://www.nucuisine.com/retail/norris.html",
-	42.053,-87.674,"http://www.nucuisine.com/retail/plaza.html",
-	42.053,-87.678,"http://www.nucuisine.com/retail/foster.html",
-	42.051,-87.675,"http://www.nucuisine.com/retail/crowe.html",
-	42.052,-87.681,"http://www.nucuisine.com/retail/frans.html",
-	42.050,-87.675,"http://www.nucuisine.com/retail/hinman.html",
-	);
+	42.053,-87.672,"http://nucuisine.com/retail/norris.html",
+	42.053,-87.674,"http://nucuisine.com/retail/plaza.html",
+	42.053,-87.678,"http://nucuisine.com/retail/foster.html",
+	42.051,-87.675,"http://nucuisine.com/retail/crowe.html",
+	42.052,-87.681,"http://nucuisine.com/retail/frans.html",
+	42.050,-87.675,"http://nucuisine.com/retail/hinman.html");
 
 function GPSonSuccess(position) {
 	var element = document.getElementById('geolocation');
@@ -37,5 +36,13 @@ var locdistance = function(x,y){
 }
 
 function autoselect(){
+	var i;
+	var tempdistance=locdistance(courtinfo[0],courtinfo[1]);
+	var tempindex=0;
+	for(i=0;i<document.getElementById('foodcourtselect').length;i++){
+		if(locdistance(courtinfo[3*i],courtinfo[3*i+1])<=tempdistance)
+			tempindex=i;
+	}
+	alert(courtinfo[3*tempindex+2]);
 	document.getElementById("foodcourt4").selected=true;
 }
