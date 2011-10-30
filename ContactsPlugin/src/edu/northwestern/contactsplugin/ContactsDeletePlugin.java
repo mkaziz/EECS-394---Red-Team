916b90734a12a5292b2dac21927eb83eb637a362
Khalid Aziz <kaziz@u.northwestern.edu>
2011-10-30 02:27:56 -0500

    Added new project for making plugin

---
+++ ContactsPlugin/src/edu/northwestern/contactsplugin/ContactsDeletePlugin.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package edu.northwestern.contactsplugin;
+
+import org.json.JSONArray;
+
+import com.phonegap.api.Plugin;
+import com.phonegap.api.PluginResult;
+
+/**
+ * @author khalid
+ *
+ */
+public class ContactsDeletePlugin extends Plugin {
+
+	/* (non-Javadoc)
+	 * @see com.phonegap.api.Plugin#execute(java.lang.String, org.json.JSONArray, java.lang.String)
+	 */
+	@Override
+	public PluginResult execute(String arg0, JSONArray arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
