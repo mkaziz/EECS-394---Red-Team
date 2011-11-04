1155ebc1027bcb6d705f0b99cb4a43e6cc2eb9bb
Khalid Aziz <kaziz@u.northwestern.edu>
2011-11-03 21:02:58 -0500

    Now pulls a list of contacts and displays them in add contacts page.

--- NotePadKhalid/assets/www/contacts.js
+++ NotePadKhalid/assets/www/contacts.js
@@ -1,11 +1,30 @@
 // Add Contact Functions
 
 function fetchContacts() {
-	navigator.contacts.find(["id","name"], callbackSuccess, errorCB, null);
+	navigator.contacts.find(["id","name"], createContactsList, errorCB, null);
 }
 
 function callbackSuccess(contacts) {
 	alert("Found " + contacts.length + " contacts.");
+}
+
+/**
+ * Creates a str with all the notes in <li> elements, and sets that
+ * as the HTML of the appropriate <ul> element. 
+ * NOTE: <ul> element must be refreshed to apply jQuery Mobile markup
+ */
+function createContactsList(contacts) {
+	var len = contacts.length;
+	var outputStr = "";
+
+	for (var i=0; i<len; i++){
+		outputStr += "<li data-icon='plus'><a href = 'index.html' rel='external'>" //onclick='openNotePage("+results.rows.item(i).note_id+"); return false;'>" + 
+					 + contacts[i].name.givenName + " " + contacts[i].name.familyName
+					 + "</a></li>";
+	}
+
+	$("#listOfContacts").html(outputStr);
+	$("#listOfContacts").listview("refresh");
 }
 
 // GENERIC FUNCTIONS
@@ -22,5 +41,6 @@ function successCB() {
  * Generic error callback function
  */
 function errorCB(err) {
+	alert("error");
 	alert("Error: "+err.code+" msg: "+err.message);
 }
