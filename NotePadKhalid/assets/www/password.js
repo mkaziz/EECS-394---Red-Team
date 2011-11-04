afc7f8f8af5542171ae1b1f59674b239ba3ae22a
Cassandra Rommel <cassandra.rommel@gmail.com>
2011-11-04 09:21:09 -0500

    Password functions rightpassword() and nopassword() set. As set password page has been created

--- NotePadKhalid/assets/www/password.js
+++ NotePadKhalid/assets/www/password.js
@@ -1,11 +1,28 @@
- function rightpassword() {
-	//matches calculator entry with expected password
+    function setpassword()  
+    {  
+    //allows the user to set a password
+        localStorage.setItem("mypassword", "123");  
+    	localStorage["mypassword"] = "123";  
+    	alert('Your password has been set to' + localStorage.getItem("mypassword"));  
+    } 
 
-password appState = ((password)getApplicationContext());
-String checky = appState.getpassword();
 
-alert ('Can you see %s', checky);
-
-		if (document.calculator.ans.value == "1")
+function rightpassword() {
+//matches calculator entry with expected password 
+	
+	if (nopassword()) { //check if a password exists
+		if (document.calculator.ans.value == localStorage.getItem("mypassword"))
 			window.location.href = "home.html";
+	}
+	else
+		setpassword(); //if a password doesn't exists, set one
+}
+
+function nopassword() {
+//checks if the user has set a password yet
+	if(localStorage.getItem("mypassword")=="NULL") {
+		return 0;	
+	}
+	return 1;
+
 }
