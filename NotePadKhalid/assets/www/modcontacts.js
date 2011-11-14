8b74b6f7dab6fb6bfe4c8db30884327981e7ae0b
Haotian Liu <haotianliu@wireless-165-124-205-208.nuwlan.northwestern.edu>
2011-11-14 12:27:07 -0600

    We can display all the secret contacts and delete numbers one by one

--- NotePadKhalid/assets/www/modcontacts.js
+++ NotePadKhalid/assets/www/modcontacts.js
@@ -1,27 +1,25 @@
 function FindContacts(){
-	alert("we are in FindContacts!");
+	
 	var db = window.openDatabase("secrets", "1.0", "Secret Contacts", 500000);
 		db.transaction(
         function(tx) {
         	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
 			tx.executeSql('CREATE TABLE IF NOT EXISTS contacts (contactId integer primary key, name varchar(200),  unique(name))');
 			tx.executeSql('CREATE TABLE IF NOT EXISTS numbers (contactId integer not null, number integer not null, foreign key(contactId) references contacts(contactId))');
-			tx.executeSql('SELECT * FROM contacts', [], 
+			tx.executeSql('SELECT * FROM contacts, numbers WHERE contacts.contactId = numbers.contactId', [], 
 				function(tx, results) {
 					//alert("we are here!"+results.rows.item(0).givenName);
 					var output = "";
+					alert("we are in FindContacts!");
 					for (var i = 0; i < results.rows.length; i++){
 						var Name 		= results.rows.item(i).name;
 						var contactId 	= results.rows.item(i).contactId;
+						var Numbers		= results.rows.item(i).number;
 						//var number		= results.rows.item(i).numbers[0];
 						alert(contactId + ", " + Name);
-		//				output += 	"<li>" +
-		//								"<a data-role='button' href=\"tel: 5554\" rel=external>" + 
-		//									  Name + 
-		//								"</a>" +
-		//								"<a data-role='button' data-icon='delete' data-theme=\"a\" onclick='ModifyContacts(\"" + contactId + "\",\"" + Name + "\");' rel=external>" + 
-		//								"</a>";
-						output +=	"<div data-role=\"collapsible\">" +
+						if((i>0) && (results.rows.item(i).contactId != results.rows.item(i-1).contactId))
+						{
+							output +=	"<div data-role=\"collapsible\">" +
 									"<h3>" + Name + "</h3>" +
 									"<ul data-role=\"listview\" data-inset=\"true\">" + 
 										"<fieldset class=\"ui-grid-a\">" +
@@ -29,23 +27,48 @@ function FindContacts(){
 												"<button type=\"submit\" data-icon=\"gear\"	 data-theme=\"c\" onclick=\"alert('click');\">Modify</button>" +
 											"</div>" +
 											"<div class=\"ui-block-b\">" +
-												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"alert('click');\">Delete</button>" +
+												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"DeleteContacts(" + contactId + ", " + Name + ");\">Delete</button>" +
 											"</div>" +
 										"</fieldset>";
-						tx.executeSql('SELECT * FROM numbers WHERE contactId=?'
-							, [contactId], function(tx, results) {
-								alert("output");
-								for (var j = 0; j < results.rows.length; j++){
-									var Numbers	= results.rows.item(i).number;
-									alert(contactId + ", " + Numbers);
-									output += "<li>" +
-													"<a href=\"tel:" + Numbers + "\">CALL</a><a data-role='button' data-icon='delete' data-theme="a" onclick="alert('click');">del</a>" +
-												"</li>";
-								}
-								alert(output);
-							}, errorCB);
-						output += "</ul></div>";
-						alert(output);
+						}
+						else if(i==0)
+						{
+							output +=	"<div data-role=\"collapsible\">" +
+									"<h3>" + Name + "</h3>" +
+									"<ul data-role=\"listview\" data-inset=\"true\">" + 
+										"<fieldset class=\"ui-grid-a\">" +
+											"<div class=\"ui-block-a\">" +
+												"<button type=\"submit\" data-icon=\"gear\"	 data-theme=\"c\" onclick=\"alert('click');\">Modify</button>" +
+											"</div>" +
+											"<div class=\"ui-block-b\">" +
+												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"DeleteContacts(" + contactId + ", " + Name + ");\">Delete</button>" +
+											"</div>" +
+										"</fieldset>";
+						}
+						output += "<li>" +
+										"<a href=\"tel:" + Numbers + "\">CALL:"+ Numbers + "</a><a data-role='button' data-icon='delete' data-theme=\"a\" onclick=\"DeleteNumber(" + Numbers + ");\">del</a>" +
+									"</li>";
+		//				tx.executeSql('SELECT * FROM numbers WHERE contactId=?'
+		//					, [results.rows.item(i).contactId], function(tx, results) {
+		//						alert(results.rows.item(0).number);
+		//						for (var j = 0; j < results.rows.length; j++){
+		//							var Numbers	= results.rows.item(i).number;
+		//							alert(contactId + ", " + Numbers);
+		//							output += "<li>" +
+		//											"<a href=\"tel:" + Numbers + "\">CALL</a><a data-role='button' data-icon='delete' data-theme=\"a\" onclick=\"alert('click');\">del</a>" +
+		//										"</li>";
+		//						}
+		//						alert(output);
+		//					}, errorCB);
+						if((i+1 < results.rows.length) && (results.rows.item(i).contactId != results.rows.item(i+1).contactId))
+						{
+							output += "</ul></div>";
+						}
+						else if(i+1 == results.rows.length)
+						{
+							output += "</ul></div>";
+						}
+					//	alert(output);
 					}
 			//		$("#secretlist").html(output);
 			//		$("#secretlist").listview("refresh");
@@ -65,11 +88,46 @@ function ModifyContacts(contactId,Name){
 
 function DeleteContacts(contactId,Name){
 	var db = window.openDatabase("secrets", "1.0", "Secret Contacts", 500000);
-	
+	var r = confirm("Delete "+ Name + "!");
+	if (r == true)
+	{
 		db.transaction(
-        function(tx) {
-        	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
+       	function(tx) {
+	       	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
 			tx.executeSql('CREATE TABLE IF NOT EXISTS contacts (contactId integer primary key, name varchar(200),  unique(name))');
 			tx.executeSql('DELETE FROM contacts WHERE contactId=?', [contactId], [], errorCB);
 		});
+		db.transaction(
+        function(tx) {
+        	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS numbers (contactId integer not null, number integer not null, foreign key(contactId) references contacts(contactId))');
+			tx.executeSql('DELETE FROM numbers WHERE contactId=?', [contactId], [], errorCB);
+		});
+		FindContacts();
+		alert("OK! " + Name + " Deleted!");
+	}
+	else
+	{
+		alert("No action taken!");
+	}
+}
+
+function DeleteNumber(Number){
+	var db = window.openDatabase("secrets", "1.0", "Secret Contacts", 500000);
+	var r = confirm("Delete "+ Number + "!");
+	if (r == true)
+	{
+		db.transaction(
+        function(tx) {
+        	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS numbers (contactId integer not null, number integer not null, foreign key(contactId) references contacts(contactId))');
+			tx.executeSql('DELETE FROM numbers WHERE number=?', [Number], [], errorCB);
+		});
+		FindContacts();
+		alert("OK! " + Number + " Deleted!");
+	}
+	else
+	{
+		alert("No action taken!");
+	}
 }
