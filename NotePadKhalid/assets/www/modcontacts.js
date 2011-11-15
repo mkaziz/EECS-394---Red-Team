9f1e6ccaf9ffec09769e1d4e919e72d83b5c0f45
Haotian Liu <haotianliu@Haotians-MacBook-Pro.local>
2011-11-14 21:14:14 -0600

    Fix the delete bug
    We can delete phone number one by one.
    If the contacts only has one number, deleting the number will also
    delete the contact.

--- NotePadKhalid/assets/www/modcontacts.js
+++ NotePadKhalid/assets/www/modcontacts.js
@@ -16,8 +16,8 @@ function FindContacts(){
 						var contactId 	= results.rows.item(i).contactId;
 						var Numbers		= results.rows.item(i).number;
 						//var number		= results.rows.item(i).numbers[0];
-						alert(contactId + ", " + Name);
-						if((i>0) && (results.rows.item(i).contactId != results.rows.item(i-1).contactId))
+						//alert(contactId + ", " + Name);
+						if((i==0) || ((i>0) && (results.rows.item(i).contactId != results.rows.item(i-1).contactId)))
 						{
 							output +=	"<div data-role=\"collapsible\">" +
 									"<h3>" + Name + "</h3>" +
@@ -27,44 +27,24 @@ function FindContacts(){
 												"<button type=\"submit\" data-icon=\"gear\"	 data-theme=\"c\" onclick=\"alert('click');\">Modify</button>" +
 											"</div>" +
 											"<div class=\"ui-block-b\">" +
-												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"DeleteContacts(" + contactId + ", " + Name + ");\">Delete</button>" +
+												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"DeleteContacts('" + contactId + "','" + Name + "');\">Delete</button>" +
 											"</div>" +
 										"</fieldset>";
 						}
-						else if(i==0)
-						{
-							output +=	"<div data-role=\"collapsible\">" +
-									"<h3>" + Name + "</h3>" +
-									"<ul data-role=\"listview\" data-inset=\"true\">" + 
-										"<fieldset class=\"ui-grid-a\">" +
-											"<div class=\"ui-block-a\">" +
-												"<button type=\"submit\" data-icon=\"gear\"	 data-theme=\"c\" onclick=\"alert('click');\">Modify</button>" +
-											"</div>" +
-											"<div class=\"ui-block-b\">" +
-												"<button type=\"submit\" data-icon=\"delete\" data-theme=\"b\" onclick=\"DeleteContacts(" + contactId + ", " + Name + ");\">Delete</button>" +
-											"</div>" +
-										"</fieldset>";
+						var onlyone;// check whether it is the only number! 1: it is the onlyone, 2: there are several
+						if(i == 0){
+							onlyone = (results.rows.item(0).contactId != results.rows.item(1).contactId);
+						}
+						else if(i == results.rows.length - 1){
+							onlyone = (results.rows.item(i).contactId != results.rows.item(i-1).contactId);
+						}
+						else{
+							onlyone = (results.rows.item(i).contactId != results.rows.item(i-1).contactId) && (results.rows.item(i).contactId != results.rows.item(i+1).contactId);
 						}
 						output += "<li>" +
-										"<a href=\"tel:" + Numbers + "\">CALL:"+ Numbers + "</a><a data-role='button' data-icon='delete' data-theme=\"a\" onclick=\"DeleteNumber(" + Numbers + ");\">del</a>" +
+										"<a href=\"tel:" + Numbers + "\">CALL:"+ Numbers + "</a><a data-role='button' data-icon='delete' data-theme=\"a\" onclick=\"DeleteNumber(" + contactId + "," + Numbers + "," + onlyone + ");\">del</a>" +
 									"</li>";
-		//				tx.executeSql('SELECT * FROM numbers WHERE contactId=?'
-		//					, [results.rows.item(i).contactId], function(tx, results) {
-		//						alert(results.rows.item(0).number);
-		//						for (var j = 0; j < results.rows.length; j++){
-		//							var Numbers	= results.rows.item(i).number;
-		//							alert(contactId + ", " + Numbers);
-		//							output += "<li>" +
-		//											"<a href=\"tel:" + Numbers + "\">CALL</a><a data-role='button' data-icon='delete' data-theme=\"a\" onclick=\"alert('click');\">del</a>" +
-		//										"</li>";
-		//						}
-		//						alert(output);
-		//					}, errorCB);
-						if((i+1 < results.rows.length) && (results.rows.item(i).contactId != results.rows.item(i+1).contactId))
-						{
-							output += "</ul></div>";
-						}
-						else if(i+1 == results.rows.length)
+						if((i+1 == results.rows.length) || ((i+1 < results.rows.length) && (results.rows.item(i).contactId != results.rows.item(i+1).contactId)))
 						{
 							output += "</ul></div>";
 						}
@@ -92,8 +72,8 @@ function DeleteContacts(contactId,Name){
 	if (r == true)
 	{
 		db.transaction(
-       	function(tx) {
-	       	//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
+     	function(tx) {
+       		//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
 			tx.executeSql('CREATE TABLE IF NOT EXISTS contacts (contactId integer primary key, name varchar(200),  unique(name))');
 			tx.executeSql('DELETE FROM contacts WHERE contactId=?', [contactId], [], errorCB);
 		});
@@ -112,7 +92,7 @@ function DeleteContacts(contactId,Name){
 	}
 }
 
-function DeleteNumber(Number){
+function DeleteNumber(contactId,Number,onlyone){
 	var db = window.openDatabase("secrets", "1.0", "Secret Contacts", 500000);
 	var r = confirm("Delete "+ Number + "!");
 	if (r == true)
@@ -123,6 +103,15 @@ function DeleteNumber(Number){
 			tx.executeSql('CREATE TABLE IF NOT EXISTS numbers (contactId integer not null, number integer not null, foreign key(contactId) references contacts(contactId))');
 			tx.executeSql('DELETE FROM numbers WHERE number=?', [Number], [], errorCB);
 		});
+		if(onlyone){
+			alert(onlyone);
+			db.transaction(
+	     	function(tx) {
+	       		//tx.executeSql('DROP TABLE IF EXISTS secretContacts');
+				tx.executeSql('CREATE TABLE IF NOT EXISTS contacts (contactId integer primary key, name varchar(200),  unique(name))');
+				tx.executeSql('DELETE FROM contacts WHERE contactId=?', [contactId], [], errorCB);
+			});
+		}
 		FindContacts();
 		alert("OK! " + Number + " Deleted!");
 	}
