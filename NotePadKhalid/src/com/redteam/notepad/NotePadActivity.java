dad634408f5b9a00fe24c1b44bcd476db1109e49
Haotian Liu <haotianliu@Haotians-MacBook-Pro.local>
2011-11-17 20:37:11 -0600

    Back to 2 days ago, the mod contacts works again.
    I am not sure the changes after that version are still included. I
    would check the log to figure out.

--- NotePadKhalid/src/com/redteam/notepad/NotePadActivity.java
+++ NotePadKhalid/src/com/redteam/notepad/NotePadActivity.java
@@ -8,10 +8,6 @@ public class NotePadActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-=======
-        super.loadUrl("file:///android_asset/www/modcontacts.html");
->>>>>>> ac2d5f913369fd41bed9d8523506eb1256fe0873
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/home.html");
     }
 }
